15828f8307a387984cb57741f9df76055a76cfb9
Bernadoadk <bernado.adikpeto@epitech.eu>
2026-05-27 23:41:59 +0100

    update

--- app/db.server.ts
+++ app/db.server.ts
@@ -2,49 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 declare global {
   // eslint-disable-next-line no-var
-  var prismaGlobal: PrismaClient;
+  var __db__: PrismaClient | undefined;
 }
 
-function getDatabaseUrl() {
-  const databaseUrl = process.env.DATABASE_URL;
-
-  if (!databaseUrl || !process.env.VERCEL) {
-    return databaseUrl;
-  }
-
-  const url = new URL(databaseUrl);
-
-  if (url.protocol !== "postgresql:" && url.protocol !== "postgres:") {
-    return databaseUrl;
-  }
-
-  if (!url.searchParams.has("connection_limit")) {
-    url.searchParams.set("connection_limit", "1");
-  }
-
-  if (!url.searchParams.has("pool_timeout")) {
-    url.searchParams.set("pool_timeout", "30");
-  }
-
-  return url.toString();
+if (!global.__db__) {
+  global.__db__ = new PrismaClient();
 }
 
-function createPrismaClient() {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: getDatabaseUrl(),
-      },
-    },
-  });
-}
-
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prismaGlobal) {
-    global.prismaGlobal = createPrismaClient();
-  }
-}
-
-const prisma = global.prismaGlobal ?? createPrismaClient();
+const prisma = global.__db__;
 
 export default prisma;
